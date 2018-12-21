bebb9bafd99b3c190c848f34642484b559f4b18a
Kamya Domnick <domisemak@yahoo.co.uk>
2018-12-21 06:19:37 +0300

    feat(favourite): user able to favourite article
    - user favourites an article incase he had not had not
    - user unfavourites an article incase he already did
    
    [Delivers #130635935]

--- authors/apps/articles/views.py
+++ authors/apps/articles/views.py
@@ -1,9 +1,11 @@
-from authors.apps.articles.models import Article, Comment
+from authors.apps.articles.models import Article, Comment, FavoriteArticle
 from rest_framework import generics
-from .serializers import ArticleSerializer, CommentSerializer
+from .serializers import (ArticleSerializer, CommentSerializer,
+                          FavoriteSerializer)
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.generics import (RetrieveUpdateDestroyAPIView, ListAPIView)
+from rest_framework.generics import RetrieveUpdateDestroyAPIView, ListAPIView
+from rest_framework.permissions import IsAuthenticated
 
 
 class ArticleCreateView(generics.ListCreateAPIView):
@@ -12,7 +14,7 @@ class ArticleCreateView(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
-        return Response({"message": "article created successfully"}, status=status.HTTP_201_CREATED)
+        return Response({"message": "article created successfully"},status = status.HTTP_201_CREATED)
 
 
 class DetailsView(generics.RetrieveUpdateDestroyAPIView):
@@ -61,3 +63,72 @@ class CommentListAPIView(ListAPIView):
 
     def get(self, request, pk):
         return Response(status=status.HTTP_200_OK)
+
+
+class FavouriteArticleAPIView(generics.CreateAPIView, generics.DestroyAPIView):
+    """Incase the user feels satisfied with the article, he can favourite it
+    and incase he feels disatisfied with the article he can Unfavourite it. """
+
+    serializer_class = FavoriteSerializer
+    permission_classes = (IsAuthenticated, )
+
+    def article_exists(self, art_slug):
+        try:
+            article = Article.objects.get(art_slug=art_slug)
+        except Article.DoesNotExist:
+            return Response({
+                'Message': 'The article does not exist'
+            }, status.HTTP_404_NOT_FOUND)
+        return article
+
+    def post(self, request, art_slug):
+        """
+       Implement article favorite  or unfavorite
+       """
+
+        article = self.article_exists(art_slug)
+
+        favorited = FavoriteArticle.objects.filter(
+            user=request.user.id, article=article.id).exists()
+
+        if favorited:
+            return Response(
+                {
+                    'Message': "You have already favourited this article"
+                }, status.HTTP_400_BAD_REQUEST)
+
+        data = {"article": article.id, "user": request.user.id}
+        serializer = self.serializer_class(data=data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(
+            {
+                "Message": "You have successfully favorited this article."
+            }, status.HTTP_200_OK)
+
+    def delete(self, request, art_slug):
+        """
+       Implement article favorite  or unfavorite
+       """
+
+        article = self.article_exists(art_slug)
+
+        favorited = FavoriteArticle.objects.filter(
+            user=request.user.id, article=article.id).exists()
+
+        if not favorited:
+            return Response(
+                {
+                    'Message': "You have already unfavourited this article"
+                }, status.HTTP_400_BAD_REQUEST)
+
+        instance = FavoriteArticle.objects.filter(
+            user=request.user.id, article=article.id)
+
+        self.perform_destroy(instance)
+
+        return Response(
+            {
+                "Message": "You have successfully unfavorited this article."
+            }, status.HTTP_200_OK)
--- authors/apps/authentication/urls.py
+++ authors/apps/authentication/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path, re_path
 from .views import (LoginAPIView, RegistrationAPIView,
-                    SocialAuthAPIView, ActivationView
+                    SocialAuthAPIView, ActivationView,
                     )
 
 urlpatterns = [
     path('users/', RegistrationAPIView.as_view(), name='register'),
-    path('users/login/', LoginAPIView.as_view()),
     path('users/login/', LoginAPIView.as_view(), name="login"),
     path('login/oauth/', SocialAuthAPIView.as_view(), name="social_auth"),
     re_path(r'^user/activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[a-zA-Z0-9\-_]+?\.[a-zA-Z0-9\-_]+?\.([a-zA-Z0-9\-_]+))/',
             ActivationView.as_view(), name='activate'),
     path('socialAuth/', SocialAuthAPIView.as_view()),
+
 ]
